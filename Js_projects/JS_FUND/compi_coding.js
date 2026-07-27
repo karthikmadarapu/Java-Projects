426cfb413524136af634a0aec8aaacc8b7d9f660
Karthik Madarapu <karthikmadarapu21@gmail.com>
2026-07-26 20:51:26 -0400

    glued the trapping rain water in mind

--- Js_projects/JS_FUND/compi_coding.js
+++ Js_projects/JS_FUND/compi_coding.js
@@ -1,3 +1,5 @@
+
+
 for(let i=1; i<=100;i++){
     
      if(i % 3 === 0 && i % 5=== 0){
@@ -14,6 +16,42 @@ for(let i=1; i<=100;i++){
      }
 
 }
+
+
+
+
+// trapping water 
+
+function trappingRainWater(array){
+   
+   let left = array[0], right = array.length - 1;
+   let left_Max = 0, right_Max = 0, water_units = 0 ;
+   
+   while(left < right){
+
+      if(array[left] < array[right]){
+
+         array[left] >=  left_Max ? (left_Max = array[left] ) : (water_units += left_Max - array[left]);
+        
+         left++;
+
+      }
+      else{
+          array[right] >=  right_Max ? (right_Max = array[right] ) : (water_units += right_Max - array[right]);
+        
+         right--;
+      }
+
+   }
+
+   return water_units ;
+
+
+}
+
+const exArray =  [0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1];
+
+console.log(trappingRainWater(exArray));
 
 
 
--- Js_projects/advanced.js
+++ Js_projects/advanced.js
@@ -206,7 +206,7 @@ function trapIt(array){
 
 }
 
-const exArray = [0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1];
+ const exArray = [0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1];
 console.log(trapIt(exArray));
 
 
