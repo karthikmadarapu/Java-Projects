90bfb6954d35b731050f6883201406e1f15ac97b
Karthik Madarapu <karthikmadarapu21@gmail.com>
2026-07-30 21:34:30 -0400

    trapping rain water revision

--- Js_projects/JS_FUND/compi_coding.js
+++ Js_projects/JS_FUND/compi_coding.js
@@ -22,36 +22,69 @@ for(let i=1; i<=100;i++){
 
 // trapping water 
 
-function trappingRainWater(array){
+// function trappingRainWater(array){
    
-   let left = array[0], right = array.length - 1;
-   let left_Max = 0, right_Max = 0, water_units = 0 ;
+//    let left = array[0], right = array.length - 1;
+//    let left_Max = 0, right_Max = 0, water_units = 0 ;
    
-   while(left < right){
+//    while(left < right){
 
-      if(array[left] < array[right]){
+//       if(array[left] < array[right]){
 
-         array[left] >=  left_Max ? (left_Max = array[left] ) : (water_units += left_Max - array[left]);
+//          array[left] >=  left_Max ? (left_Max = array[left] ) : (water_units += left_Max - array[left]);
         
-         left++;
+//          left++;
 
+//       }
+//       else{
+//           array[right] >=  right_Max ? (right_Max = array[right] ) : (water_units += right_Max - array[right]);
+        
+//          right--;
+//       }
+
+//    }
+
+//    return water_units ;
+
+
+// }
+
+// const exArray =  [0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1];
+
+// console.log(trappingRainWater(exArray));
+
+
+
+function trappingWater(myArr){
+   
+   let leftP = myArr[0], rightP = myArr.length - 1;
+   let leftmaxx = 0, rightmaxx = 0, waterUnits = 0;
+
+   while(leftP < rightP){
+
+      if(myArr[leftP] < myArr[rightP]){
+         myArr[leftP] >= leftmaxx ? (leftmaxx = myArr[leftP]) : (waterUnits += leftmaxx - myArr[leftP]);
+         leftP++ ;
       }
+
       else{
-          array[right] >=  right_Max ? (right_Max = array[right] ) : (water_units += right_Max - array[right]);
-        
-         right--;
+         myArr[rightP] >= rightmaxx ? (rightmaxx = myArr[rightP]) : (waterUnits += rightmaxx - myArr[rightP]);
+         rightP-- ;
       }
+
+
 
    }
 
-   return water_units ;
+   return waterUnits;
 
 
 }
 
-const exArray =  [0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1];
 
-console.log(trappingRainWater(exArray));
+ const exArray =  [0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1];
+
+ console.log(trappingWater(exArray));
 
 
 
