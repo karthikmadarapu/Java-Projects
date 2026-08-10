fda750f2efa358a44bfb735ba6862a9bddcd8159
Karthik Madarapu <karthikmadarapu21@gmail.com>
2026-08-10 13:58:03 -0400

    successfully implemented stat.js (memory of the calculator program!!!!!!

---
+++ calculator-cli/calculator/js/state.js
@@ -0,0 +1,44 @@
+let currentValue = "";
+let previousValue = "";
+let operator = null;
+// for Reading the values!!!
+function getCurrentValue(){
+    return currentValue;
+}
+
+function getPreviousValue(){
+    return previousValueValue;
+}
+
+function getOperator(){
+    return operator;
+}
+
+// for setting  or updating new values 
+function setCurrentValue(value){
+        currentValue = value;
+}
+
+function setPreviousValue(value){
+        previousValue = value;
+}
+
+function setOperator(value){
+        operator = value;
+}
+
+export {
+    getCurrentValue,
+    getPreviousValue,
+    getOperator,
+    setCurrentValue,
+    setPreviousValue,
+    setOperator
+};
+
+
+// setCurrentValue(7);
+
+
+
+// console.log(getCurrentValue());
