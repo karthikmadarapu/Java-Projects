56de230aa4ebfa817e4fdb25a83e1d762fc05dda
Karthik Madarapu <karthikmadarapu21@gmail.com>
2026-08-17 13:21:01 -0400

    filter array method

--- Js_projects/JS_FUND/prep_program_5months/array_methods_prep.js
+++ Js_projects/JS_FUND/prep_program_5months/array_methods_prep.js
@@ -3,3 +3,12 @@ const arrayPrices = [10, 120, 20, 30, 40];
 const afterDiscountPrice = arrayPrices.map(eachNum => eachNum * 0.80);
 
 console.log(afterDiscountPrice);
+
+// array filter method 
+
+const filterNums = [1, 4, 7, 10, 15, 18];
+
+const evenNum = filterNums.filter(each => each % 2 === 0);
+
+console.log(evenNum);
+
