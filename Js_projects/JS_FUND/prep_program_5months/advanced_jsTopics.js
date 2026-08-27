ff4e5db7b6e1a6eb45b5bd5ded1030c6afc5a9a9
Karthik Madarapu <karthikmadarapu21@gmail.com>
2026-08-27 14:39:26 -0400

    try catch block for strip club entry

--- Js_projects/JS_FUND/prep_program_5months/advanced_jsTopics.js
+++ Js_projects/JS_FUND/prep_program_5months/advanced_jsTopics.js
@@ -15,5 +15,27 @@
 
  console.log("final application data status : ", data);
 
+
+
+//  Strip Club entry Bouncer
+let age;
+ try {
+  age = 18;
+
+  if(age <= 19) throw new Error(`your age must be 19+ to enter the CLUB`);
+
+
+
+  console.log(`Access Granted`);
+
+ }
+
+ catch(error){
+
+    console.log(`error caught successfully :`, error.message);
+
+ }
+
+
  
- 
+
