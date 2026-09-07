e08a5f3dab555ed7636de9b1094658e327b5b230
Karthik Madarapu <karthikmadarapu21@gmail.com>
2026-09-07 10:21:05 -0400

    password validator

--- Js_projects/JS_FUND/prep_program_5months/moreAdvanced.js
+++ Js_projects/JS_FUND/prep_program_5months/moreAdvanced.js
@@ -20,3 +20,40 @@ async function receiveOrder(){
 receiveOrder();
 
 
+// Password authenticator
+
+const checkCredentials = (password) => new Promise((resolve, reject) => {
+    if (password === "secret123") {
+        resolve("🔐 Access Granted");
+    } else {
+        reject("❌ Invalid Password!");
+    }
+});
+
+
+async function login(userPassword){
+    
+    try{
+        if(!userPassword || userPassword.trim() === ""){
+            throw new Error("Password cannot be empty !");
+        }
+        
+
+        console.log("=====PASSWORD CHECKING VERY STRICT=====");
+        const passConfirm = await checkCredentials(userPassword);
+
+        console.log(passConfirm);
+    }
+    catch(err){
+        console.log("err: " ,err.message|| err);
+    }
+    finally{
+        console.log("FUNCTION PASSED EFFECIENTLY\n");
+    }
+
+}
+
+
+login("secret123");
+
+
